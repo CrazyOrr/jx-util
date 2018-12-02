0b51c4c2f97bd7cdf55ce94c052c1278c56c2511
7 <fengzilijie@qq.com>
2018-12-02 21:32:31 +0800

    2.0.6

---
+++ tests/test-signals.js
@@ -0,0 +1,34 @@
+var signals = require('../lib/support/signals')
+
+var expect = require('chai').expect
+
+describe('signals 功能测试', function() {
+
+  describe('常规', function() {
+    let ret = ''
+    let obj = {
+      dataSignal: new signals.Signal()
+    }
+
+    function onReceived(param1, param2) {
+      ret = param1+param2
+    }
+
+    obj.dataSignal.send(1, 4)
+
+
+    obj.dataSignal.on(onReceived)
+
+    obj.dataSignal.off(onReceived)
+
+    obj.dataSignal.dispose()
+
+    // obj.dataSignal.send(6) // 会异常
+    // obj.dataSignal.on(onReceived) // 会抛异常
+
+    it(`测试 bind`, function() {
+
+      expect(ret === 5).to.be.equal(true)
+    })
+  })
+})
